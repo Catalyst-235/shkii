d28d5241b3e3671b27ddecef6728faf49cbf9abc
Catalyst-235 <81960015+Catalyst-235@users.noreply.github.com>
2025-06-24 17:14:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,22 @@
-const CACHE_NAME = 'barcode-v2';
+const CACHE_NAME = 'barcode-offline-v3';
 const URLS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
   '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
   'https://cdn.jsdelivr.net/npm/jsbarcode@3.11.5/dist/JsBarcode.all.min.js'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(URLS_TO_CACHE))
+      .then((cache) => cache.addAll(URLS_TO_CACHE))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then((response) => response || fetch(event.request))
   );
 });
