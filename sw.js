ead6e48b480f73d72617ee6ac601df8f8804d3ff
Catalyst-235 <81960015+Catalyst-235@users.noreply.github.com>
2025-06-24 16:37:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,15 @@
-const CACHE_NAME = 'barcode-generator-v4';
-const urlsToCache = [
+const CACHE_NAME = 'barcode-v1';
+const URLS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
   'https://cdn.jsdelivr.net/npm/jsbarcode@3.11.5/dist/JsBarcode.all.min.js'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => cache.addAll(URLS_TO_CACHE))
   );
 });
 
@@ -20,24 +18,4 @@ self.addEventListener('fetch', event => {
     caches.match(event.request)
       .then(response => response || fetch(event.request))
   );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener('message', event => {
-  if (event.data.action === 'skipWaiting') {
-    self.skipWaiting();
-  }
 });
